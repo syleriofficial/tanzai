7b4a826fd4202ddc94180d5fb3817db128ab4132
syleriofficial <syleri.official@gmail.com>
2026-05-08 18:37:00 +0530

    Add files via upload

---
+++ tanzai-ai-v11/components/Footer.jsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export default function Footer(){return <footer className="footer"><div><b>Tanzai AI by Syleri</b><br/>Powered by Syleri Engine.</div><div className="row"><Link href="/privacy">Privacy</Link><Link href="/terms">Terms</Link><Link href="/contact">Contact</Link></div></footer>}
