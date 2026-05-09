b65b66bfd04bbd59fe8c7c534aab82e12ecaf43b
syleriofficial <syleri.official@gmail.com>
2026-05-09 06:07:06 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,1 +1,7 @@
-export { default } from "./Navvar";
+export default function Navbar() {
+  return (
+    <nav className="w-full px-6 py-4 bg-black text-white font-bold text-xl">
+      Tanzai AI
+    </nav>
+  );
+}
