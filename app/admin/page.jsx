a6ed2ef9c08df7b5ca817f6d64766063b09b8e82
syleriofficial <syleri.official@gmail.com>
2026-05-09 11:53:59 +0530

    Delete app directory

--- app/admin/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Admin(){
- return(
-  <div style={{padding:40}}>
-   <h1>Syleri Admin Panel</h1>
-  </div>
- )
-}
--- app/developer/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Developer(){
- return(
-  <div style={{padding:40}}>
-   <h1>Syleri API</h1>
-   <p>Developer platform powered by Syleri Engine.</p>
-  </div>
- )
-}
