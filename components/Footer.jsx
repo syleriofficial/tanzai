a2cb7bcd8c75db554457e03770614df31adfe160
syleriofficial <syleri.official@gmail.com>
2026-05-09 06:51:00 +0530

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,2 +0,0 @@
-import Link from "next/link";
-export default function Footer(){return <footer className="footer"><div><b>Tanzai AI by Syleri</b><br/>Powered by Syleri Engine.</div><div className="row"><Link href="/privacy">Privacy</Link><Link href="/terms">Terms</Link><Link href="/contact">Contact</Link></div></footer>}
--- components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="w-full px-6 py-4 bg-black text-white font-bold text-xl">
-      Tanzai AI
-    </nav>
-  );
-}
