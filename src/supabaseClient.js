111bd77e6c485b6390c7f0694b34dce496345b38
syleriofficial <syleri.official@gmail.com>
2026-05-15 13:13:01 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://fzopbexdrfzxyhetmsrt.supabase.co";
 
 // Paste your Supabase Publishable key here. Do NOT paste Secret key.
-const supabaseAnonKey = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+const supabaseAnonKey = "sb_publishable_e88m2FmDuPmg0BiSNOjWQQ_DkZGilNq";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
